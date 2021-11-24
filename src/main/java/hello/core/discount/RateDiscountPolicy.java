73f26876d71cc2a1a1725a900402c6f884eefd80
gwangwoo <rhkddn5161@naver.com>
2021-11-24 09:00:57 +0900

    빈생명주기콜백)53강_빈등록초기화_소멸메소드

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
-    private  int discountPercent = 10;
+    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
         if(member.getGrade() == Grade.VIP) {
